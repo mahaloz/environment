1b368c83f470f7b45ecfb750be8b3ae92453023b
mahaloz <zionbasque@gmail.com>
2022-11-05 20:48:54 -0700

    update regolith installer

--- configs/regolith/install.sh
+++ configs/regolith/install.sh
@@ -1,4 +1,13 @@
 cp .Xresources ~/.Xresources
 mkdir -p ~/.config/regolith2 
 cp Xresources ~/.config/regolith2/
+
+# background
 cp cyber.jpg ~/Pictures/cyber.jpg
+
+# make the the top bar look better
+sudo apt remove i3xrocks-app-launcher i3xrocks-net-traffic i3xrocks-info i3xrocks-next-workspace
+
+# terminal colors
+sudo apt-get install dconf-cli uuid-runtime -y
+bash -c "$(wget -qO- https://git.io/vQgMr)"
